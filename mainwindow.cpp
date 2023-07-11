321baebd1a4c14fab4109766ed9176f9c3db963f
Bogdan Tverdoy <bogdan.tverdoy@gmail.com>
2023-07-11 06:54:38 +0400

    gui cosmetic change. add xmp lib folders

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <iostream>
+#include <fstream>
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
